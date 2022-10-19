f4cf473913cd59e1a287ce2bc1ebe67d4c95ea51
mlite <mlite@users.noreply.github.com>
2022-10-19 12:26:05 -0700

    ..

--- main.c
+++ main.c
@@ -1,95 +1,28 @@
 #include "httpd.h"
-#include <sys/stat.h>
-
-#define CHUNK_SIZE 1024 // read 1024 bytes at a time
-
-// Public directory settings
-#define PUBLIC_DIR "./public"
-#define INDEX_HTML "/index.html"
-#define NOT_FOUND_HTML "/404.html"
-
-int main(int c, char **v) {
-  char *port = c == 1 ? "8000" : v[1];
+int main(int c, char **argv){
+  char *port = argv[1];
   serve_forever(port);
   return 0;
 }
 
-int file_exists(const char *file_name) {
-  struct stat buffer;
-  int exists;
+void route(){
+  if( strcmp("/", uri) == 0 ){
+    printf("HTTP/1.1 200 OK\n");
+    printf("Access-Control-Allow-Origin: *\n");
+    //printf("Content-Length: %d\n", strlen(resp));
+    printf("Cache-Control: no-cache\n\n");
 
-  exists = (stat(file_name, &buffer) == 0);
-
-  return exists;
-}
-
-int read_file(const char *file_name) {
-  char buf[CHUNK_SIZE];
-  FILE *file;
-  size_t nread;
-  int err = 1;
-
-  file = fopen(file_name, "r");
-
-  if (file) {
-    while ((nread = fread(buf, 1, sizeof buf, file)) > 0)
-      fwrite(buf, 1, nread, stdout);
-
-    err = ferror(file);
-    fclose(file);
-  }
-  return err;
-}
-
-void route() {
-  ROUTE_START()
-
-  GET("/") {
-    char index_html[20];
-    sprintf(index_html, "%s%s", PUBLIC_DIR, INDEX_HTML);
-
-    HTTP_200;
-    if (file_exists(index_html)) {
-      read_file(index_html);
-    } else {
-      printf("Hello! You are using %s\n\n", request_header("User-Agent"));
+    read_file("index.html");
+  }else{
+    char *file = uri + 1;
+    if( does_file_exist(file) ){
+      printf("HTTP/1.1 200 OK\n");
+      printf("Access-Control-Allow-Origin: *\n");
+      //printf("Content-Length: %d\n", strlen(resp));
+      printf("Cache-Control: no-cache\n\n");
+      read_file(file);
+    }else{
+      printf("HTTP/1.1 404 Not found\n\n");
     }
   }
-
-  GET("/test") {
-    HTTP_200;
-    printf("List of request headers:\n\n");
-
-    header_t *h = request_headers();
-
-    while (h->name) {
-      printf("%s: %s\n", h->name, h->value);
-      h++;
-    }
-  }
-
-  POST("/") {
-    HTTP_201;
-    printf("Wow, seems that you POSTed %d bytes.\n", payload_size);
-    printf("Fetch the data using `payload` variable.\n");
-    if (payload_size > 0)
-      printf("Request body: %s", payload);
-  }
-
-  GET(uri) {
-    char file_name[255];
-    sprintf(file_name, "%s%s", PUBLIC_DIR, uri);
-
-    if (file_exists(file_name)) {
-      HTTP_200;
-      read_file(file_name);
-    } else {
-      HTTP_404;
-      sprintf(file_name, "%s%s", PUBLIC_DIR, NOT_FOUND_HTML);
-      if (file_exists(file_name))
-        read_file(file_name);
-    }
-  }
-
-  ROUTE_END()
 }
